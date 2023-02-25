4ab365c6b1584744861579b161d994eae47da505
user <user@localhost.localdomain>
2023-02-25 14:20:08 +0900

    Modify Script

--- buildasm.sh
+++ buildasm.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+echo "MeowOS Build Script"
+echo "Build Destination Clean.."
+rm -rd -f ./dist
+mkdir dist
+
+echo "NASM Compile Working.."
 nasm Loader.asm -f bin -o ./dist/loader.bin
 nasm ExtendedMain.asm -f bin -o ./dist/extended.bin
 
+echo "Merging binaries to floppy image.."
 cd ./dist
-
 cat loader.bin extended.bin > meowos.flp
--- qemuboot.sh
+++ qemuboot.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd dist
+
+echo "MeowOS Boot Script"
+echo "Starting qemu-kvm.."
 /usr/libexec/qemu-kvm -m 512M -cpu host -smp sockets=1,cores=1,threads=1 -fda meowos.flp
+
+echo "Stopped!"
